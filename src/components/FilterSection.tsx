0dfd1f072395a47ac6a3bd1d832b6072e2854d2e
Niko <soder.n@gmail.com>
2023-09-13 13:22:34 +0300

    Add remove and add to active tags functionality

--- src/components/FilterSection.tsx
+++ src/components/FilterSection.tsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
 const FilterSection = () => {
-  return <section className="container mx-auto">FilterSection</section>;
+  const [activeTags, setActiveTags] = useState<string[]>([]);
+  const [genres, setGenres] = useState([
+    "Genre 1",
+    "Genre 2",
+    "Genre 3",
+    "Genre 4",
+    "Genre 5",
+    "Genre 6",
+    "Genre 7",
+    "Genre 8",
+    "Genre 9",
+  ]);
+
+  const addToActiveTags = (genre: string) => {
+    // if tag is not already chosen
+    if (!activeTags.includes(genre)) {
+      setActiveTags([...activeTags, genre]);
+    }
+  };
+
+  const removeFromActiveTags = (genre: string) => {
+    setActiveTags((state) => state.filter((item) => item !== genre));
+  };
+
+  return (
+    <section className="container mx-auto">
+      <section className="flex justify-around">
+        {/* active tags */}
+        <ul className="flex max-w-sm flex-wrap gap-4 bg-zinc-800 p-4">
+          {activeTags.length ? (
+            activeTags.map((tag) => (
+              <li className="flex bg-zinc-900" key={Math.random()}>
+                <p>{tag}</p>
+                <button onClick={() => removeFromActiveTags(tag)}>
+                  <XMarkIcon className="h-6 w-6" />
+                </button>
+              </li>
+            ))
+          ) : (
+            <li>No tags selected</li>
+          )}
+        </ul>
+        {/* choose genre */}
+        <ul>
+          {genres.map((genre) => (
+            <li key={genre}>
+              <button onClick={() => addToActiveTags(genre)}>{genre}</button>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </section>
+  );
 };
 
 export default FilterSection;
