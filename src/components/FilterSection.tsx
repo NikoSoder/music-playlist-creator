cb2c03081e5b50c90d63e50ba1f5f0531925a3f8
Niko <soder.n@gmail.com>
2023-09-13 14:45:20 +0300

    Add some styling to active tags

--- src/components/FilterSection.tsx
+++ src/components/FilterSection.tsx
@@ -8,16 +8,16 @@ const FilterSection = () => {
     "Genre 2",
     "Genre 3",
     "Genre 4",
-    "Genre 5",
-    "Genre 6",
-    "Genre 7",
-    "Genre 8",
+    "Genre 54954758475",
+    "Genre 65735",
+    "Genre 70080282028028",
+    "Genre 832",
     "Genre 9",
   ]);
 
   const addToActiveTags = (genre: string) => {
     // if tag is not already chosen
-    if (!activeTags.includes(genre)) {
+    if (!activeTags.includes(genre) && activeTags.length < 6) {
       setActiveTags([...activeTags, genre]);
     }
   };
@@ -30,20 +30,28 @@ const FilterSection = () => {
     <section className="container mx-auto">
       <section className="flex justify-around">
         {/* active tags */}
-        <ul className="flex max-w-sm flex-wrap gap-4 bg-zinc-800 p-4">
-          {activeTags.length ? (
-            activeTags.map((tag) => (
-              <li className="flex bg-zinc-900" key={Math.random()}>
-                <p>{tag}</p>
-                <button onClick={() => removeFromActiveTags(tag)}>
-                  <XMarkIcon className="h-6 w-6" />
-                </button>
-              </li>
-            ))
-          ) : (
-            <li>No tags selected</li>
-          )}
-        </ul>
+        <div className="w-full max-w-sm">
+          <ul className="flex w-full flex-wrap gap-2 bg-zinc-800 p-4">
+            {activeTags.length ? (
+              activeTags.map((tag) => (
+                <li
+                  className="flex items-center justify-center gap-2 rounded bg-zinc-900 px-3 py-2"
+                  key={Math.random()}
+                >
+                  <p>{tag}</p>
+                  <button
+                    className="rounded-full bg-zinc-700 p-1"
+                    onClick={() => removeFromActiveTags(tag)}
+                  >
+                    <XMarkIcon className="h-4 w-4" />
+                  </button>
+                </li>
+              ))
+            ) : (
+              <li>No tags selected</li>
+            )}
+          </ul>
+        </div>
         {/* choose genre */}
         <ul>
           {genres.map((genre) => (
