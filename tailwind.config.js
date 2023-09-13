e6a233812780d8dc175bd9d4cee0c4364809eca0
Niko <soder.n@gmail.com>
2023-09-13 12:09:02 +0300

    Add new shadow to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        blue: "0 0 25px -5px #43b7ff",
+      },
+    },
   },
   plugins: [],
 };
