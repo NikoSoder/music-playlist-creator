7d87aa9627b60075a126f5be4c9f3105b5895c2d
NikoSoder <105541058+NikoSoder@users.noreply.github.com>
2023-09-19 12:50:47 +0300

    Feature/tags (#1)
    * Add remove and add to active tags functionality
    
    * Add some styling to active tags
    
    * Create filters section layout
    
    * Close other dropdowns when filter is clicked
    
    * Add styling to active tags
    
    * Add clear all button
    
    * Refactor FilterSection component

--- src/components/FilterSection.tsx
+++ src/components/FilterSection.tsx
@@ -1,5 +1,103 @@
+import { useState } from "react";
+import Tags from "./Tags";
+import Filters from "./Filters";
+
 const FilterSection = () => {
-  return <section className="container mx-auto">FilterSection</section>;
+  const [activeTags, setActiveTags] = useState<string[]>([]);
+  const [genres, setGenres] = useState([
+    "Genre 1",
+    "Genre 2",
+    "Genre 3",
+    "Genre 4",
+    "Genre 54954758475",
+    "Genre 65735",
+    "Genre 70080282028028",
+    "Genre 832",
+    "Genre 9",
+  ]);
+  const [decade, setDecade] = useState([
+    "70s",
+    "80s",
+    "90s",
+    "2000s",
+    "2010s",
+    "2020s",
+  ]);
+  const [toggleGenres, setToggleGenres] = useState(false);
+  const [toggleDecades, setToggleDecades] = useState(false);
+
+  const addToActiveTags = (filter: string) => {
+    if (!activeTags.includes(filter) && activeTags.length < 6) {
+      setActiveTags([...activeTags, filter]);
+    } else {
+      // remove from activeTags if already chosen
+      setActiveTags((state) => state.filter((item) => item !== filter));
+    }
+  };
+
+  const removeFromActiveTags = (filter: string) => {
+    setActiveTags((state) => state.filter((item) => item !== filter));
+  };
+
+  const handleFilterDropdowns = (filter: string) => {
+    // close other dropdowns when one filter is clicked
+    // todo: make this function cleaner
+    if (filter === "Genre" && toggleGenres) {
+      return setToggleGenres(!toggleGenres);
+    }
+    if (filter === "Decade" && toggleDecades) {
+      return setToggleDecades(!toggleDecades);
+    }
+    if (filter === "Genre") {
+      setToggleGenres(true);
+      setToggleDecades(false);
+    } else {
+      setToggleDecades(true);
+      setToggleGenres(false);
+    }
+  };
+
+  return (
+    <section className="container mx-auto">
+      <section className="flex justify-around">
+        {/* active tags */}
+        <Tags
+          activeTags={activeTags}
+          handleRemoveFromActiveTags={removeFromActiveTags}
+        />
+        {/* choose filters */}
+        <section className="w-full max-w-sm bg-zinc-800 p-4">
+          <div className="mb-4 flex items-center justify-between text-white">
+            <h2 className="text-lg">Filters</h2>
+            <button
+              className="hover:underline"
+              onClick={() => setActiveTags([])}
+            >
+              Clear all
+            </button>
+          </div>
+          <div className="space-y-1">
+            <Filters
+              title="Genre"
+              activeTags={activeTags}
+              handleFilterDropdowns={handleFilterDropdowns}
+              handleAddToActiveTags={addToActiveTags}
+              filterArray={genres}
+              isToggleActive={toggleGenres}
+            />
+            <Filters
+              title="Decade"
+              activeTags={activeTags}
+              handleFilterDropdowns={handleFilterDropdowns}
+              handleAddToActiveTags={addToActiveTags}
+              filterArray={decade}
+              isToggleActive={toggleDecades}
+            />
+          </div>
+        </section>
+      </section>
+    </section>
+  );
 };
 
 export default FilterSection;
---
+++ src/components/Filters.tsx
@@ -0,0 +1,61 @@
+import { CheckIcon } from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+
+interface ChildPropsFilters {
+  title: string;
+  activeTags: string[];
+  handleFilterDropdowns: (filter: string) => void;
+  handleAddToActiveTags: (filter: string) => void;
+  filterArray: string[];
+  isToggleActive: boolean;
+}
+
+const Filters = ({
+  title,
+  activeTags,
+  handleFilterDropdowns,
+  handleAddToActiveTags,
+  filterArray,
+  isToggleActive,
+}: ChildPropsFilters) => {
+  return (
+    <>
+      <button
+        onClick={() => handleFilterDropdowns(title)}
+        className="flex w-full items-center justify-between"
+      >
+        <p className="font-semibold">{title}</p>
+        <ChevronDownIcon
+          className={`h-5 w-5 transition-all ${isToggleActive && "rotate-180"}`}
+        />
+      </button>
+      {isToggleActive && (
+        <ul>
+          {filterArray.map((filter) => (
+            <li key={filter}>
+              <button
+                className="relative"
+                onClick={() => handleAddToActiveTags(filter)}
+              >
+                <p
+                  className={`ps-6 ${
+                    activeTags.includes(filter) && "text-blue-300"
+                  }`}
+                >
+                  {filter}
+                </p>
+                {activeTags.includes(filter) && (
+                  <span className="absolute inset-y-0 left-0 top-0 flex items-center">
+                    <CheckIcon className="h-5 w-5 text-blue-300" />
+                  </span>
+                )}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default Filters;
