b9f7c73f0b6cf64bd6d1ef68fc500c1152ef51c1
Niko <soder.n@gmail.com>
2023-09-21 14:32:37 +0300

    Add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,29 @@ export default {
       boxShadow: {
         blue: "0 0 14px -5px #43b7ff",
       },
+      keyframes: {
+        appear: {
+          "0%": { transform: "scale(0.2)" },
+          "100%": { transform: "scale(1)" },
+        },
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        below: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(70px)",
+          },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        appear:
+          "appear 700ms 700ms backwards cubic-bezier(0.37, -0.8, 0.26, 1.45)",
+        fadeIn: "fadeIn 1s 1s backwards",
+        below: "below 1s cubic-bezier(.58, -0.06 ,.3, 1.5)",
+      },
     },
   },
   plugins: [],
