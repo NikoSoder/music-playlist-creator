432d397e7dc30ed06ca77af90e755cf863774113
Niko <soder.n@gmail.com>
2023-09-21 14:37:00 +0300

    Add mock artists data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import FilterSection from "./components/FilterSection";
+import Modal from "./components/Modal";
 
 function App() {
   return (
     <main className="p-4">
       <Header />
       <FilterSection />
+      <Modal />
     </main>
   );
 }
---
+++ src/mockdata/mockArtists.tsx
@@ -0,0 +1,52 @@
+export const fakeArtistData = [
+  {
+    song: "Hotel California",
+    artist: "Eagles",
+    year: 1976,
+  },
+  {
+    song: "Boogie Wonderland",
+    artist: "Earth, Wind & Fire",
+    year: 1979,
+  },
+  {
+    song: "Yesterday",
+    artist: "The Beatles",
+    year: 1965,
+  },
+  {
+    song: "Stairway to Heaven",
+    artist: "Led Zeppelin",
+    year: 1971,
+  },
+  {
+    song: "Rolling in the Deep",
+    artist: "Adele",
+    year: 2010,
+  },
+  {
+    song: "Bohemian Rhapsody",
+    artist: "Queen",
+    year: 1975,
+  },
+  {
+    song: "Imagine",
+    artist: "John Lennon",
+    year: 1971,
+  },
+  {
+    song: "Like a Rolling Stone",
+    artist: "Bob Dylan",
+    year: 1965,
+  },
+  {
+    song: "Smells Like Teen Spirit",
+    artist: "Nirvana",
+    year: 1991,
+  },
+  {
+    song: "Billie Jean",
+    artist: "Michael Jackson",
+    year: 1983,
+  },
+];
