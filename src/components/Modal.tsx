c0186d2b62fe6b6387c847d7aea43cdedff12765
NikoSoder <105541058+NikoSoder@users.noreply.github.com>
2023-09-26 16:19:54 +0300

    Feature/modal (#2)
    * Add animations
    
    * Create Modal.tsx
    
    * Add mock artists data
    
    * Add close button to modal
    
    * Close modal when clicking outside of it
    
    * Fix modal closing when clicking inside of modal
    
    * Remove useless classes

---
+++ src/components/Modal.tsx
@@ -0,0 +1,61 @@
+import { CheckIcon } from "@heroicons/react/24/outline";
+import { fakeArtistData } from "../mockdata/mockArtists";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+interface ChildPropsModal {
+  handleCloseModal: () => void;
+}
+
+const Modal = ({ handleCloseModal }: ChildPropsModal) => {
+  return (
+    <>
+      {/* modal background window */}
+      <section
+        onClick={handleCloseModal}
+        className="fixed left-0 top-0 h-full w-full bg-zinc-900/90 p-2"
+      ></section>
+      {/* close modal when clicking outside of it or don't close */}
+      {/* backdrop-blur-sm lagging ^ */}
+      <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <section className="max-w-lg animate-below rounded-md border border-blue-300 bg-slate-800/80 px-10 py-6 shadow-blue">
+          {/* modal header */}
+          <div className="relative mb-4 flex flex-col items-center justify-center gap-2">
+            <div className="animate-appear rounded-full border-2 border-blue-300 p-1 shadow-blue">
+              <CheckIcon className="h-7 w-7 text-blue-300" />
+            </div>
+            <h2 className="animate-fadeIn text-xl">Playlist created!</h2>
+            <button
+              onClick={handleCloseModal}
+              className="absolute right-0 top-0 animate-fadeIn rounded-md hover:bg-slate-700"
+            >
+              <XMarkIcon className="h-6 w-6 text-slate-300 hover:text-white" />
+            </button>
+          </div>
+          {/* modal body */}
+          <div className="flex animate-fadeIn justify-center">
+            <table className="border-separate border-spacing-4">
+              <thead>
+                <tr>
+                  <th className="text-start">Song</th>
+                  <th className="text-start">Artist</th>
+                  <th className="text-start">Year</th>
+                </tr>
+              </thead>
+              <tbody>
+                {fakeArtistData.map((artist) => (
+                  <tr key={artist.song}>
+                    <td>{artist.song}</td>
+                    <td>{artist.artist}</td>
+                    <td>{artist.year}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
