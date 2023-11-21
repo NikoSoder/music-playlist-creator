9844cebb0b7f68541d00987f651ab9328fe9fe6c
Niko <soder.n@gmail.com>
2023-11-21 14:29:18 +0200

    Show previous playlists on page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import FilterSection from "./components/FilterSection";
 import Modal from "./components/Modal";
 import { useState } from "react";
 import { Song } from "./types/response";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 function App() {
   const [isOpenModal, setIsOpenModal] = useState(false); // set this to true when playlist is created
   const [isPlaylistFetchLoading, setIsPlaylistFecthLoading] = useState(false);
   const [userPlaylist, setUserPlaylist] = useState<Song[]>([]);
   const [APIResponseMessage, setAPIResponseMessage] = useState("");
+  const oldPlaylists = useLocalStorage();
 
   const closeModal = (playlist: Song[]) => {
     setIsOpenModal(false);
@@ -36,6 +38,28 @@ function App() {
           responseMessage={APIResponseMessage}
         />
       )}
+      {/* old playlists */}
+      <section>
+        <button
+          onClick={oldPlaylists.clearLocalStorage}
+          className="bg-slate-50 text-black"
+        >
+          remove localstorage
+        </button>
+        <h1 className="text-2xl">Previous Playlists</h1>
+        {oldPlaylists.previousPlaylists.map((playlist, index) => (
+          <div className="mb-2 border" key={index}>
+            <h2>{playlist.playlistName}</h2>
+            <p>{new Date(playlist.createdAt).toLocaleDateString()}</p>
+            {playlist.songs.map((song) => (
+              <div key={song.song_id}>
+                <p>{song.artist_name}</p>
+                <p>{song.song_name}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </section>
     </main>
   );
 }
