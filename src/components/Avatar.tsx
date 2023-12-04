42d726090b85face65bc57970bbad20027e83162
Niko <soder.n@gmail.com>
2023-12-04 12:58:09 +0200

    Move Avatar and Spotify auth link to own component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,23 @@
+interface ChildPropsAvatar {
+  name?: string;
+  image?: string;
+}
+
+const Avatar = (props: ChildPropsAvatar) => {
+  return (
+    <div className="flex gap-2">
+      {props.image ? (
+        <figure className="h-7 w-7 overflow-hidden rounded-full ring ring-sky-800 ring-offset-2 ring-offset-zinc-900 md:h-8 md:w-8">
+          <img
+            className="w-full"
+            src={props.image}
+            alt="spotify profile picture"
+          />
+        </figure>
+      ) : null}
+      <p>{props.name}</p>
+    </div>
+  );
+};
+
+export default Avatar;
