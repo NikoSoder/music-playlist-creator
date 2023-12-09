9544ed0d7415cb535836188d9f0beee5b9995a89
Niko <soder.n@gmail.com>
2023-12-09 17:30:28 +0200

    Add shadcn accordion to previous playlists

--- src/components/PreviousPlaylists.tsx
+++ src/components/PreviousPlaylists.tsx
@@ -1,8 +1,12 @@
 import { PreviousPlaylists } from "../types/previousplaylists";
 import { Song } from "../types/response";
-import { PlusIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
-
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "./ui/button";
 interface ChildPropsPreviousPlaylists {
   previousPlaylists: PreviousPlaylists[];
   addPlaylistToLocalStorage: (playlist: Song[]) => void;
@@ -10,16 +14,6 @@ interface ChildPropsPreviousPlaylists {
 }
 
 const RecentPlaylists = (props: ChildPropsPreviousPlaylists) => {
-  const [expandedPlaylists, setExpandedPlaylists] = useState<number>();
-
-  const togglePlaylist = (index: number) => {
-    if (index === expandedPlaylists) {
-      setExpandedPlaylists(undefined);
-    } else {
-      setExpandedPlaylists(index);
-    }
-  };
-
   if (props.previousPlaylists.length === 0) {
     return (
       <section className="container mx-auto py-20 text-center">
@@ -33,45 +27,30 @@ const RecentPlaylists = (props: ChildPropsPreviousPlaylists) => {
     <section className="container mx-auto py-20">
       <div className="mb-8 flex items-center justify-center gap-6">
         <h1 className="text-2xl">Previous Playlists</h1>
-        <button onClick={props.clearLocalStorage} className="hover:underline">
+        <Button variant={"outline"} onClick={props.clearLocalStorage}>
           Clear all
-        </button>
+        </Button>
       </div>
-      <section className="flex flex-col items-center justify-center gap-4">
+      <Accordion type="single" collapsible className="w-full">
         {props.previousPlaylists.map((playlist, index) => (
-          <div
-            className="w-full rounded-md border-2 border-zinc-500 p-6 md:w-[600px]"
-            key={index}
-          >
-            <div
-              onClick={() => togglePlaylist(index)}
-              className="cursor-pointer border-b border-zinc-700 pb-2 text-xl"
-            >
-              <div className="flex items-center justify-between">
+          <AccordionItem key={index} value={index.toString()}>
+            <AccordionTrigger>
+              <div>
                 <h2>{playlist.playlistName}</h2>
-                <PlusIcon
-                  className={`h-7 w-7 transition-all ${
-                    expandedPlaylists === index && "rotate-45"
-                  }`}
-                />
+                <p>{new Date(playlist.createdAt).toLocaleDateString()}</p>
               </div>
-              <p>{new Date(playlist.createdAt).toLocaleDateString()}</p>
-            </div>
-            <div
-              className={`${
-                expandedPlaylists === index ? "max-h-[900px]" : "max-h-0"
-              } flex flex-col gap-4 overflow-hidden pt-2 transition-all duration-500 ease-in-out`}
-            >
+            </AccordionTrigger>
+            <AccordionContent className="flex flex-col gap-4">
               {playlist.songs.map((song) => (
                 <div key={song.song_id}>
                   <p>{song.artist_name}</p>
                   <p>{song.song_name}</p>
                 </div>
               ))}
-            </div>
-          </div>
+            </AccordionContent>
+          </AccordionItem>
         ))}
-      </section>
+      </Accordion>
     </section>
   );
 };
