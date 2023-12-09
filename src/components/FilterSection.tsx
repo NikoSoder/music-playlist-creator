8ee49694dbdc0b42116579250f48b6ec268f407f
Niko <soder.n@gmail.com>
2023-12-09 17:32:11 +0200

    Style filter section with shadcn

--- src/components/FilterSection.tsx
+++ src/components/FilterSection.tsx
@@ -5,10 +5,30 @@ import { getPlaylist } from "../api/api_service";
 import { APIResult } from "../types/response";
 import { Song } from "../types/response";
 import { filters } from "../shared/filters";
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { CheckIcon } from "@heroicons/react/24/outline";
-import toast, { Toaster } from "react-hot-toast";
+import { EllipsisHorizontalCircleIcon } from "@heroicons/react/24/outline";
 import { useToast } from "@/components/ui/use-toast";
+
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface ChildPropsFilterSection {
   isOpenModal: boolean;
@@ -17,6 +37,7 @@ interface ChildPropsFilterSection {
   isPlaylistFetchLoading: boolean;
   setUserPlaylist: Dispatch<React.SetStateAction<Song[]>>;
   setAPIResponseMessage: Dispatch<React.SetStateAction<string>>;
+  handleAddPlaylist: (playlist: Song[]) => void;
 }
 
 const FilterSection = ({
@@ -26,17 +47,10 @@ const FilterSection = ({
   isPlaylistFetchLoading,
   setUserPlaylist,
   setAPIResponseMessage,
+  handleAddPlaylist,
 }: ChildPropsFilterSection) => {
   const [activeTags, setActiveTags] = useState<string[]>([]);
-  const [expandedFilter, setExpandeFilter] = useState<number>();
-
-  const togglePlaylist = (index: number) => {
-    if (index === expandedFilter) {
-      setExpandeFilter(undefined);
-    } else {
-      setExpandeFilter(index);
-    }
-  };
+  const [open, setOpen] = useState(false);
   const { toast } = useToast();
 
   const createPlaylist = async () => {
@@ -93,90 +107,91 @@ const FilterSection = ({
     setActiveTags((state) => state.filter((item) => item !== filter));
   };
 
+  const clearAllActiveTags = () => {
+    setActiveTags([]);
+  };
+
   return (
+    <section className="container mx-auto border-b py-20">
       <section className="mb-16 flex flex-col items-center justify-center gap-10 md:flex-row md:items-start">
         {/* active tags */}
         <Tags
           activeTags={activeTags}
           handleRemoveFromActiveTags={removeFromActiveTags}
+          clearAllActiveTags={clearAllActiveTags}
         />
         {/* choose filters */}
-        <section className="w-full max-w-sm rounded-md border border-sky-700 bg-zinc-900 p-6 shadow-blue">
-          <div className="mb-6 flex items-center justify-between text-white">
-            <h2 className="text-lg">Filters</h2>
-            <button
-              className="hover:underline"
-              onClick={() => setActiveTags([])}
-            >
-              Clear all
-            </button>
-          </div>
-          <section className="flex flex-col items-center justify-center gap-4">
-            {filters.map((filtertype, index) => (
-              <div className="w-full rounded-md" key={index}>
-                <div
-                  onClick={() => togglePlaylist(index)}
-                  className="cursor-pointer border-b border-zinc-700 pb-2 text-xl"
-                >
-                  <div className="flex items-center justify-between">
-                    <h2>{filtertype.title}</h2>
-                    <ChevronDownIcon
-                      className={`h-5 w-5 transition-all ${
-                        expandedFilter === index && "rotate-180"
-                      }`}
-                    />
-                  </div>
-                </div>
-                <ul
-                  className={`${
-                    expandedFilter === index ? "max-h-96" : "max-h-0"
-                  } my-2 flex flex-col gap-2 overflow-hidden overflow-y-auto transition-all duration-500 ease-in-out`}
-                >
-                  {filtertype.filtersArr.map((filter, index) => (
-                    <li key={index}>
-                      <button
-                        className="relative"
-                        onClick={() => addToActiveTags(filter)}
-                      >
-                        <p
-                          className={`ps-6 ${
-                            activeTags.includes(filter) && "text-blue-200"
-                          }`}
-                        >
-                          {filter}
-                        </p>
-                        {activeTags.includes(filter) && (
-                          <span className="absolute inset-y-0 left-0 top-0 flex items-center">
-                            <CheckIcon className="h-5 w-5 text-blue-200" />
-                          </span>
-                        )}
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </section>
-        </section>
+        <div className="flex w-full max-w-sm items-center justify-between rounded-md border px-4 py-3">
+          <p className="text-sm font-medium leading-none">Search filters</p>
+          <DropdownMenu open={open} onOpenChange={setOpen}>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <EllipsisHorizontalCircleIcon className="h-8 w-8 text-muted-foreground" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-[200px]">
+              <DropdownMenuLabel>Filters</DropdownMenuLabel>
+              <DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                {filters.map((filterType, index) => (
+                  <DropdownMenuSub key={index}>
+                    <DropdownMenuSubTrigger>
+                      {filterType.title}
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuSubContent className="p-0">
+                      <Command>
+                        <CommandInput
+                          placeholder={`Filter ${filterType.title}`}
+                          autoFocus={true}
+                          className="h-9"
+                        />
+                        <CommandList>
+                          <CommandEmpty>No filters found.</CommandEmpty>
+                          <CommandGroup>
+                            {filterType.filtersArr.map((filter, index) => (
+                              <CommandItem key={index} value={filter}>
+                                <Button
+                                  className="w-full"
+                                  variant={"ghost"}
+                                  onClick={() => addToActiveTags(filter)}
+                                >
+                                  {activeTags.includes(filter) && (
+                                    <span className="absolute inset-y-0 left-2 top-0 flex items-center">
+                                      <CheckIcon className="h-5 w-5 text-blue-600 dark:text-blue-400" />
+                                    </span>
+                                  )}
+                                  {filter}
+                                </Button>
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </CommandList>
+                      </Command>
+                    </DropdownMenuSubContent>
+                  </DropdownMenuSub>
+                ))}
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </section>
       {/* create playlist button */}
       <div className="text-center">
-        <button
+        <Button
+          className="text-lg"
+          size={"lg"}
           disabled={isPlaylistFetchLoading || isOpenModal}
           onClick={createPlaylist}
-          className="group relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gradient-to-br from-cyan-500 to-blue-500 p-0.5 text-xl font-medium text-white hover:shadow-blue group-hover:from-cyan-500 group-hover:to-blue-500"
         >
           {!isPlaylistFetchLoading ? (
-            <span className="relative rounded bg-gray-900 px-10 py-3 transition-all duration-75 ease-in group-hover:bg-opacity-0">
-              Create
-            </span>
+            <p>Create</p>
           ) : (
-            <div className="relative flex items-center gap-2 rounded px-5 py-3">
+            <div className="relative flex items-center gap-2 rounded">
               <div className="h-6 w-6 animate-spin rounded-full border-4 border-cyan-700 border-t-white"></div>
               <span>Loading...</span>
             </div>
           )}
-        </button>
+        </Button>
       </div>
     </section>
   );
--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,32 +1,47 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
+import { Button } from "./ui/button";
 
 interface ChildPropsTags {
   activeTags: string[];
   handleRemoveFromActiveTags: (filter: string) => void;
+  clearAllActiveTags: () => void;
 }
 
-const Tags = ({ activeTags, handleRemoveFromActiveTags }: ChildPropsTags) => {
+const Tags = ({
+  activeTags,
+  handleRemoveFromActiveTags,
+  clearAllActiveTags,
+}: ChildPropsTags) => {
   return (
     <div className="w-full max-w-sm">
-      <ul className="flex w-full flex-wrap gap-2 rounded-md border border-sky-700 bg-zinc-900 p-6 shadow-blue">
+      <ul className="flex w-full flex-wrap gap-2 rounded-md border p-6">
         {activeTags.length ? (
-          activeTags.map((tag) => (
-            <li className="group" key={Math.random()}>
-              <button
-                className="flex items-center gap-2 rounded bg-zinc-800 px-3 py-2"
+          activeTags.map((tag, index) => (
+            <li className="group" key={index}>
+              <Button
+                variant={"secondary"}
                 onClick={() => handleRemoveFromActiveTags(tag)}
               >
                 {tag}
-                <div className="rounded-full p-1 transition-all group-hover:bg-zinc-600">
-                  <XMarkIcon className="h-4 w-4 group-hover:text-white" />
+                <div className="ms-2 rounded-full p-1 transition-all group-hover:bg-zinc-700">
+                  <XMarkIcon className="h-4 w-4" />
                 </div>
-              </button>
+              </Button>
             </li>
           ))
         ) : (
           <li>No tags selected</li>
         )}
       </ul>
+      {activeTags.length !== 0 ? (
+        <Button
+          className="text-base text-muted-foreground"
+          variant={"link"}
+          onClick={clearAllActiveTags}
+        >
+          Clear all
+        </Button>
+      ) : null}
     </div>
   );
 };
